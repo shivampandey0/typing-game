84cf01e503502511f8d0088b2f0d62dab33807bd
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-04 11:07:12 +0530

    [feat] helper functions

---
+++ src/utils/helper.js
@@ -0,0 +1,34 @@
+const alphabetsArray = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+export const getRandom = (count) => {
+  const shuffled = [...alphabetsArray].sort(() => 0.5 - Math.random());
+
+  return shuffled.slice(0, count);
+};
