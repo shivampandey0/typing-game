6d5774569857feb9ef112eddbf26bf99b10adf02
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-04 11:04:51 +0530

    [feat] Text component

---
+++ src/components/Text/Text.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Text = ({ classes, title }) => {
+  return <div className={classes}>{title}</div>;
+};
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Text } from './Text/Text';
+export { TextContainer } from './TextContainer/TextContainer';
+export { Box } from './Box/Box';
+export { InputBox } from './InputGroup/InputBox';
