215c501f40af7540cb7dfd0e685d1d1edf3398e9
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-06-04 11:05:16 +0530

    [feat] Text wrapper component

---
+++ src/components/TextContainer/TextContainer.jsx
@@ -0,0 +1,14 @@
+import { Text } from '../';
+
+export const TextContainer = () => {
+  return (
+    <>
+      <Text classes='title' title='Type The Alphabet' />
+
+      <Text
+        classes='subtitle'
+        title='Typing game to see how fast you type. Timer starts when you do :)'
+      />
+    </>
+  );
+};
